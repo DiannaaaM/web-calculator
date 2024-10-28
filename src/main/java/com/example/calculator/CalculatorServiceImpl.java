f38f0368910dd32f383218f42f71772505f454cf
DiannaaaMm <murlaevadiana@gmail.com>
2024-10-28 11:59:11 +0300

    add new files

---
+++ src/main/java/com/example/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Деление на ноль недопустимо");
+        }
+        return num1 / num2;
+    }
+}
