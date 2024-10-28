00bbb0238f52ef9e542868741ec8d6e2950fd985
DiannaaaMm <murlaevadiana@gmail.com>
2024-10-28 09:00:41 +0300

    little update methods

--- src/main/java/com/example/calculator/calculatorControler.java
+++ src/main/java/com/example/calculator/calculatorControler.java
@@ -1,36 +1,38 @@
 package com.example.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class calculatorControler {
 
-    @GetMapping("/calculator")
+    @GetMapping
     public String helloProgramm() {
         return "<h1>Добро пожаловать в калькулятор</h1>";
     }
 
-    @GetMapping("/calculator/plus?num1=5&num2=5")
+    @GetMapping("/plus")
     public String sumNumsProgramm(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 + num2;
         return "<p>" + num1 + " + " + num2 + " = " + result + "</p>";
     }
 
-    @GetMapping("/calculator/minus?num1=5&num2=5")
+    @GetMapping("/minus")
     public String minusNumsProgramm(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 - num2;
         return "<p>" + num1 + " - " + num2 + " = " + result + "</p>";
     }
 
-    @GetMapping("/calculator/multiply?num1=5&num2=5")
+    @GetMapping("/multiply")
     public String multiplyNumsProgramm(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 * num2;
         return "<p>" + num1 + " * " + num2 + " = " + result + "</p>";
     }
 
-    @GetMapping("/calculator/divide?num1=5&num2=5")
+    @GetMapping("/divide")
     public String divideNumsProgramm(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num2 == 0) {
             return "<p>Ошибка: деление на ноль</p>";
