6c363ec850b2451250c4a7af29f271fd63382696
DiannaaaMm <murlaevadiana@gmail.com>
2024-11-01 12:09:46 +0300

    add tests for operations with numbers

---
+++ src/test/java/com/example/calculator/testOperations.java
@@ -0,0 +1,46 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class testOperations {
+
+    private final CalculatorService calculatorService;
+
+
+    testOperations(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @Test
+    public void multiplyTest(){
+        int actual = calculatorService.multiply(3, 4);
+        int expected = 12;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divideTest(){
+        int actual = calculatorService.divide(10, 2);
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minusTest(){
+        int actual = calculatorService.minus(3, 4);
+        int expected = -1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plusTest(){
+        int actual = calculatorService.add(5, 5);
+        int expected = 10;
+        assertEquals(expected, actual);
+    }
+}
